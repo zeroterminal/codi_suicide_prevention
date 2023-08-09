ad1c58c63b9d108dddfb5659f051497e7b826e14
Jay <>
2023-08-09 16:06:08 +0300

    counter fix

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 // PAGES
 const home             = document.getElementById("home");
 const goal             = document.getElementById("goal");
-const gallery          = document.getElementById("gallery");
-const CTA              = document.getElementById("services");
 const contact          = document.getElementById("contact");
 
 // BUTTONS
 const home_button      = document.getElementById("home_button");
 const goal_button      = document.getElementById("goal_button");
-const gallery_button   = document.getElementById("gallery_button");
-const CTA_button       = document.getElementById("CTA_button");
 const contact_button   = document.getElementById("contact_button");
 
 // functions
@@ -19,14 +15,6 @@ home_button.onclick = function() {
 
 goal_button.onclick = function() {
     document.getElementById("goal").scrollIntoView("smooth");
-}
-
-gallery_button.onclick = function() {
-    document.getElementById("gallery").scrollIntoView("smooth");
-}
-
-CTA_button.onclick = function() {
-    document.getElementById("CTA").scrollIntoView("smooth");
 }
 
 contact_button.onclick = function() {
